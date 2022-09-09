7bdc39468d6b9acad6f919e27eabdb49d92d7d0f
JouanDeag <61161558+JouanDeag@users.noreply.github.com>
2022-09-10 01:03:27 +0200

    Formatting

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
 $(document).ready(function () {
 
-    // Set possible hello messages
-    var helloMessages = [
-        "Hello!",
-        "Hey!",
-        "Howdy!",
-        "Greetings!",
-        "Salutations!",
-        "Ciao!",
-        "Bonjour!",
-        "Hola!",
-        "Aloha!",
-        "Namaste!",
-        "Shalom!",
-        "Salaam!",
-        "Konnichiwa!",
-        "Nǐ hǎo!",
-        "Hej!",
-        "Hallå!",
-        "Hallo!"
-    ];
+	// Set possible hello messages
+	var helloMessages = [
+		"Hello!",
+		"Hey!",
+		"Howdy!",
+		"Greetings!",
+		"Salutations!",
+		"Ciao!",
+		"Bonjour!",
+		"Hola!",
+		"Aloha!",
+		"Namaste!",
+		"Shalom!",
+		"Salaam!",
+		"Konnichiwa!",
+		"Nǐ hǎo!",
+		"Hej!",
+		"Hallå!",
+		"Hallo!"
+	];
 
-    currentHello = 0;
-    function randomMsg(currentHello) {
-        // Get random message
-        let random = Math.floor(Math.random() * helloMessages.length);
+	currentHello = 0;
+	function randomMsg(currentHello) {
+		// Get random message
+		let random = Math.floor(Math.random() * helloMessages.length);
 
-        if (random == currentHello) {
-            randomMsg(currentHello);
-        } else {
-            return currentHello = random;
-        }
-    };
+		if (random == currentHello) {
+			randomMsg(currentHello);
+		} else {
+			return currentHello = random;
+		}
+	};
 
-    setInterval(() => {
+	setInterval(() => {
 
-        // Add class to animate welcome message
-        $(".welcome").addClass("hide");
+		// Add class to animate welcome message
+		$(".welcome").addClass("hide");
 
-        setTimeout(() => {
-            $(".welcome").text(helloMessages[randomMsg()]);
-            $(".welcome").removeClass("hide");
-        }, 500);
-    }, 5000);
+		setTimeout(() => {
+			$(".welcome").text(helloMessages[randomMsg()]);
+			$(".welcome").removeClass("hide");
+		}, 500);
+	}, 5000);
 });
