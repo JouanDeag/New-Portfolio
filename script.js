3500b8012b4cb23166150aeb2f8afed6d4b87b74
Johan Karlsson <johan.k0528@gmail.com>
2022-09-30 13:55:26 +0200

    Add primitive contact form (without email sending)

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
 	// Set possible hello messages
-	var helloMessages = [
+	let helloMessages = [
 		"Hello!",
 		"Hey!",
 		"Howdy!",
@@ -26,6 +26,7 @@ $(document).ready(function () {
 		// Get random message
 		let random = Math.floor(Math.random() * helloMessages.length);
 
+		// Make sure the message is different from the last one
 		if (random == currentHello) {
 			randomMsg(currentHello);
 		} else {
@@ -35,12 +36,45 @@ $(document).ready(function () {
 
 	setInterval(() => {
 
-		// Add class to animate welcome message
+		// Add class to animate fading welcome message
 		$(".welcome").addClass("hide");
 
+
+		// Change welcome message
 		setTimeout(() => {
 			$(".welcome").text(helloMessages[randomMsg()]);
 			$(".welcome").removeClass("hide");
 		}, 500);
 	}, 5000);
+
+	$('#sendEmail').click(function (e) {
+		e.preventDefault();
+
+		// Get values from form
+		const name = $('#name').val();
+		const email = $('#email').val();
+		const message = $('#message').val();
+
+		// Check if all fields are filled
+		if (name == "" || email == "" || message == "") {
+			$('#error').text("Please fill out all fields");
+			return;
+		}
+
+
+		//	If everything is good, set the error to none, set the sender to the name and reset the message form
+		$('#error').text("");
+		$('#sender').text(`${name}`);
+		$('#message').val("");
+
+
+		const notification = $('.notification');
+
+		// Change position to animate notification in and out after 2 seconds
+		notification.css({ left: '30px' });
+		setTimeout(() => {
+			notification.css({ left: '-500px' });
+		}, 2000);
+	});
+
 });
